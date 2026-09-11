fb0c40326a4ac83849b20c49a945cb775795d7e7
sba5022 <sumaiaofficial02@gmail.com>
2026-09-11 19:05:30 +0600

    vowels

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ for (let i = 1; i < numbers.length; i++) {
 }
 
 console.log(largest);
+ const text = "JavaScript is awesome";
+
+function countVowels(text) {
+  let count = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (
+      text[i] === "a" ||
+      text[i] === "e" ||
+      text[i] === "i" ||
+      text[i] === "o" ||
+      text[i] === "u"
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels(text));
