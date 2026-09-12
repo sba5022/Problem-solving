96aeeca4efe922e071e8ca91249efe7d5e66a57e
sba5022 <sumaiaofficial02@gmail.com>
2026-09-12 16:00:48 +0600

    remove dupli

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const text = "JavaScript is awesome";
 const result = text.replaceAll(" ", "");
 
 console.log(result);
+const numbers = [1, 2, 2, 3, 4, 4, 5, 5, 6];
+
+const uniqueNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (!uniqueNumbers.includes(numbers[i])) {
+    uniqueNumbers.push(numbers[i]);
+  }
+}
+
+console.log(uniqueNumbers);
