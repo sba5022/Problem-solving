0a27addc131f82bae628c9908ccd429c8fc1b56c
sba5022 <sumaiaofficial02@gmail.com>
2026-09-12 15:43:09 +0600

    text

--- index.js
+++ index.js
@@ -1,124 +1,14 @@
-// const numbers = [12, 45, 7, 89, 23, 56];
-// let largest = numbers[0];
 
-// for (let i = 1; i < numbers.length; i++) {
-//   if (numbers[i] > largest) {
-//     largest = numbers[i];
-//   }
-// }
+const text = "madam";
 
-// console.log(largest);
-//  const text = "JavaScript is awesome";
+let reversed = "";
 
-// function countVowels(text) {
-//   let count = 0;
-
-//   for (let i = 0; i < text.length; i++) {
-//     if (
-//       text[i] === "a" ||
-//       text[i] === "e" ||
-//       text[i] === "i" ||
-//       text[i] === "o" ||
-//       text[i] === "u"
-//     ) {
-//       count++;
-//     }
-//   }
-
-//   return count;
-// }
-
-console.log(countVowels(text));
-const number = [1, 2, 2, 3, 4, 4, 5, 5, 6];
-
-const uniqueNumbers = [];
-
-for (let i = 0; i < number.length; i++) {
-  if (!uniqueNumbers.includes(number[i])) {
-    uniqueNumbers.push(number[i]);
-  }
+for (let i = text.length - 1; i >= 0; i--) {
+  reversed += text[i];
 }
 
-console.log(uniqueNumbers);
-const num = [12, 45, 7, 89, 23, 56];
-
-let smallest = num[0];
-
-for (let i = 1; i < num.length; i++) {
-  if (num[i] < smallest) {
-    smallest = num[i];
-  }
+if (text === reversed) {
+  console.log("Palindrome");
+} else {
+  console.log("Not Palindrome");
 }
-
-console.log(smallest);
-// const nu = [10, 20, 30, 40, 50];
-
-// let sum = 0;
-
-// for (let i = 0; i < nu.length; i++) {
-//   sum = sum + nu[i];
-// }
-
-// console.log(sum);
-// const avg = [10, 20, 30, 40, 50];
-
-// let sum = 0;
-
-// for (let i = 0; i < avg.length; i++) {
-//   sum += avg[i];
-// }
-
-// const average = sum / avg.length;
-
-// console.log(average);
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-
-// let count = 0;
-
-// for (let i = 0; i < numbers.length; i++) {
-//   if (numbers[i] % 2 === 0) {
-//     count++;
-//   }
-// }
-
-// console.log(count);
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-
-// let count = 0;
-
-// for (let i = 0; i < numbers.length; i++) {
-//   if (numbers[i] % 2 !== 0) {
-//     count++;
-//   }
-// }
-
-// console.log(count);
-const numbers = [1, 2, 3, 4, 5];
-
-const reversed = [];
-
-for (let i = numbers.length - 1; i >= 0; i--) {
-  reversed.push(numbers[i]);
-}
-
-console.log(reversed);
-// const text = "JavaScript is awesome";
-
-// let count = 0;
-
-// for (let i = 0; i < text.length; i++) {
-//   if (
-//     text[i] === "a" ||
-//     text[i] === "e" ||
-//     text[i] === "i" ||
-//     text[i] === "o" ||
-//     text[i] === "u"
-//   ) {
-//     count++;
-//   }
-// }
-
-// console.log(count);
-const text = "JavaScript";
-
-console.log(text.length);
