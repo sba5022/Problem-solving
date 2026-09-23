d9ff1493e2e90fdab77e6d2ece14c51f84ec57c7
sba5022 <sumaiaofficial02@gmail.com>
2026-09-23 12:25:30 +0600

    fizzbizz

--- index.js
+++ index.js
@@ -76,3 +76,14 @@ if (number > 0) {
 } else {
   console.log("Zero");
 }
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
